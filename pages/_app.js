b5bcb0772d5419cb13883bfabdfd81729f69aee7
gonzarascon <gonzarascon@gmail.com>
2020-04-02 19:19:06 -0300

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <link rel="stylesheet" href="./reset.css" />
+
+        <title>Wiki Umai</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <main>Hola WikiUmai</main>;
+}
+
+export default Home;
