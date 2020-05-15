0142465ef7919b666f86eac15aafd5db8834a457
gonzarascon <gonzarascon@gmail.com>
2020-05-15 10:58:50 -0300

    Constantes con breakpoints

--- containers/Home/styles.js
+++ containers/Home/styles.js
@@ -1,21 +1,23 @@
 import styled from 'styled-components';
+import { breakpoints } from 'constants';
 
 export const ContainerMain = styled.div`
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 `;
 
 export const SectorTitulos = styled.div`
-  @media (min-width: 376px) {
+  @media (min-width: ${breakpoints.mobile}) {
     max-width: 1440px;
     width: 100%;
     margin: 0 auto;
     display: grid;
     grid-template-columns: 50%;
     grid-template-rows: 25% 150px;
-    align-items: center;  
+    align-items: center;
   }
   width: 100%;
-  display:grid;
+  display: grid;
   grid-template-columns: 100%;
   grid-template-rows: 35% 150px;
 `;
@@ -32,10 +34,10 @@ export const ContainerBanner = styled.div`
 export const Banner = styled.img;
 
 export const Titulos = styled.h1`
-  font-size: 20px;  
+  font-size: 20px;
   font-style: bold;
   color: #381460;
-  padding-top:30px;
+  padding-top: 30px;
   font-weight: bolder;
   @media (min-width: 376px) {
     font-size: 35px;
@@ -45,54 +47,50 @@ export const Textos = styled.p`
   font-size: 14px;
   color: #381460;
   margin: 3% 0%;
-  line-height:24px;
+  line-height: 24px;
   @media (min-width: 376px) {
-  font-size: 17px;
+    font-size: 17px;
   }
 `;
 
-export const ContainerUniversos = styled.div`
-
-`;
+export const ContainerUniversos = styled.div``;
 
 export const Slider = styled.div`
   display: grid;
   grid-template-columns: 33% 33% 33%;
   grid-template-rows: 100%;
   justify-content: space-around;
- 
 
-@media (min-width: 376px) {
-  display: grid;
-  grid-template-columns: 15% 15% 15% 15% 15% 15%;
-  grid-template-rows: 100%;
-  justify-content: space-around;
-}
+  @media (min-width: 376px) {
+    display: grid;
+    grid-template-columns: 15% 15% 15% 15% 15% 15%;
+    grid-template-rows: 100%;
+    justify-content: space-around;
+  }
 `;
 
 export const Card = styled.div`
- &:nth-child(n+4) {
-  display:none;
+  &:nth-child(n + 4) {
+    display: none;
   }
-    background-color: #ececec;
-    border-radius: 10px;
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    text-align: center;
-    padding: 3%;
-    @media (min-width: 376px) {
-      &:nth-child(n+3) {
-      display:block;
-  }
+  background-color: #ececec;
+  border-radius: 10px;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  text-align: center;
+  padding: 3%;
+  @media (min-width: 376px) {
+    &:nth-child(n + 3) {
+      display: block;
     }
+  }
 `;
 
 export const CardImg = styled.img`
   border-radius: 10px;
   width: 80%;
-
 `;
 
 export const TituloCard = styled.h2`
@@ -102,25 +100,25 @@ export const TituloCard = styled.h2`
   border-radius: 10px;
   text-transform: uppercase;
   font-weight: bolder;
-@media (min-width: 376px) {
-  font-size: 23px;
-}
+  @media (min-width: 376px) {
+    font-size: 23px;
+  }
 `;
 
 export const LineaDeTiempoBoton = styled.a`
-    border-radius: 20px;
-    padding: 10px 5px;
-    color: #fff;
-    text-transform: uppercase;
-    font-weight: bolder;
-    margin: 30px auto;
-    display: grid;
-    width: 85%;
-    text-align: center;
-    background-color:#FE346E;
-    font-size:13px;
+  border-radius: 20px;
+  padding: 10px 5px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bolder;
+  margin: 30px auto;
+  display: grid;
+  width: 85%;
+  text-align: center;
+  background-color: #fe346e;
+  font-size: 13px;
 
-    @media (min-width: 376px) {
+  @media (min-width: 376px) {
     border-radius: 20px;
     padding: 15px 40px;
     color: #fff;
@@ -130,6 +128,5 @@ export const LineaDeTiempoBoton = styled.a`
     display: grid;
     width: 25%;
     text-align: center;
-    }
-
+  }
 `;
