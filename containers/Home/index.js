ba735d0398a8a369bf7720d64fe439cf8e08cc27
gonzarascon <gonzarascon@gmail.com>
2020-05-28 19:57:22 -0300

    Arreglo slider

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -25,40 +25,28 @@ import { Link } from '../../components/Header/styles';
 
 function HomeContainer() {
   var settings = {
-    className: 'center',
-    centerMode: true,
-    slidesToShow: 1,
-    centerPadding: '40px',
-    slidesToScroll: 1,
-    mobileFirst: true,
-    dots: true,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 6,
-    //       slidesToScroll: 2,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 6,
-    //       initialSlide: 1,
-    //     },
-    //   },
-    // {
-    //   breakpoint: 468,
-    //   settings: {
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3,
-    //     initialSlide: 1,
-    //   },
-    // },
-    //],
+    slidesToShow: 6,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 0,
+        },
+      },
+      {
+        breakpoint: 468,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          centerPadding: '40px',
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+    ],
   };
 
   return (
