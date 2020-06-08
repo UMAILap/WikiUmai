472af03b88ef6d409ca28e0f864ee809ae5cd6ab
sharoncazap <sharicazap@gmail.com>
2020-06-08 20:53:29 -0300

    Lógica en Pasos --> pills Planetas, colorFondo

--- components/PasosPersonaje/PasoUno/index.js
+++ components/PasosPersonaje/PasoUno/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import {
   PasoContainer,
@@ -18,10 +18,7 @@ import {
 } from './styles';
 
 function PasoUno() {
-  const [ 
-    selectedPlanet, 
-    setSelectedPlanet
-  ] = useState(null);
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
 
   function handleClickPlaneta(planet_name) {
     setSelectedPlanet(planet_name);
@@ -47,32 +44,32 @@ function PasoUno() {
           <AvatarPlaneta>
             <AvatarPlanetaTitulo>Planeta del personaje</AvatarPlanetaTitulo>
             <Planetas>
-              <Planeta 
-                colorFondo={selectedPlanet === 'wereld' ? true : false} 
+              <Planeta
+                colorFondo={selectedPlanet === 'wereld' ? true : false}
                 onClick={() => handleClickPlaneta('wereld')}>
                 Wereld
               </Planeta>
-              <Planeta 
+              <Planeta
                 colorFondo={selectedPlanet === 'zoro' ? true : false}
                 onClick={() => handleClickPlaneta('zoro')}>
                 Zoro
               </Planeta>
-              <Planeta 
+              <Planeta
                 colorFondo={selectedPlanet === 'sol' ? true : false}
                 onClick={() => handleClickPlaneta('sol')}>
                 Sol
               </Planeta>
-              <Planeta 
+              <Planeta
                 colorFondo={selectedPlanet === 'ikigai' ? true : false}
                 onClick={() => handleClickPlaneta('ikigai')}>
                 Ikigai
               </Planeta>
-              <Planeta 
+              <Planeta
                 colorFondo={selectedPlanet === 'nima' ? true : false}
                 onClick={() => handleClickPlaneta('nima')}>
                 Nima
               </Planeta>
-              <Planeta 
+              <Planeta
                 colorFondo={selectedPlanet === 'adhera' ? true : false}
                 onClick={() => handleClickPlaneta('adhera')}>
                 Adhera
