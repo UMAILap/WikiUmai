086c39584a1cd3c0ca9a56cc7ce1b346c19a9a7b
sofia-code <51446456+sofia-code@users.noreply.github.com>
2020-07-16 15:28:24 -0300

    Commit tecnologias/historias universo / planetas

---
+++ pages/tecnologias-universo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  Layout,
+  TecnologiasUniverso as TecnologiasUniversoContainer,
+} from 'containers';
+
+function TecnologiasUniverso() {
+  return (
+    <Layout>
+      <TecnologiasUniversoContainer />
+    </Layout>
+  );
+}
+
+export default TecnologiasUniverso;
