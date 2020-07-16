927433743f284c8bc3403a3d12e34b69c00d6d5f
dieguito711 <diegosalischiker711@gmail.com>
2020-07-16 16:10:36 -0300

    BotonVioleta y CardUniverso con next/router

--- components/BotonVioleta/index.js
+++ components/BotonVioleta/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { useRouter } from 'next/router';
 import { VioletaBoton } from './styles';
 
-function BotonVioleta({ children, isColorPink, isBotonRosa }) {
+function BotonVioleta({ children, isColorPink, isBotonRosa, href }) {
+  const router = useRouter();
+  const handleClick = e => {
+    e.preventDefault();
+    router.push(href);
+  };
   return (
-    <VioletaBoton isColorPink={isColorPink} isBotonRosa={isBotonRosa}>
+    <VioletaBoton
+      isColorPink={isColorPink}
+      isBotonRosa={isBotonRosa}
+      href={href}
+      onClick={handleClick}>
       {children}
     </VioletaBoton>
   );
--- components/CardUniverso/index.js
+++ components/CardUniverso/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { useRouter } from 'next/router';
 import { UniversoCard } from './styles';
 
-function CardUniverso({ children }) {
-  return <UniversoCard>{children}</UniversoCard>;
+function CardUniverso({ children, href }) {
+  const router = useRouter();
+  const handleClick = e => {
+    e.preventDefault();
+    router.push(href);
+  };
+  return (
+    <UniversoCard href={href} onClick={handleClick}>
+      {children}
+    </UniversoCard>
+  );
 }
 CardUniverso.propTypes = {
   children: PropTypes.element.isRequired,
