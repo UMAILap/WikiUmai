43105f45debfca5759c144ff7d4c25f8fabd7cb9
Nayla <nayla.mar2@gmail.com>
2020-10-08 18:01:23 -0300

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -2,18 +2,13 @@ import React from 'react';
 import { Layout } from 'containers';
 import Imagen404 from '../public/images/404.png';
 
-function My404() { 
-  return(
-
+function My404() {
+  return (
     <Layout>
-      
       <img src={Imagen404} />
       <h1>La página solicitada no fue encontrada. Viajero perdido! </h1>
-
     </Layout>
-  
-
-  ) 
+  );
 }
 
 export default My404;
