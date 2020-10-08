ffc3bbbfd13025bc79a32c9d8b1e101c730f9c6d
rodriandrade <rodriandrade1498@gmail.com>
2020-10-08 16:22:20 -0300

    Planetas getServerSideProps - Home

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { getDataCollection } from 'utils';
 import { Layout, Home as HomeContainer } from 'containers';
 
-function Home({ data }) {
+function Home({ data, planetas }) {
   const isHome = true;
   return (
     <Layout>
-      <HomeContainer data={data}/>
+      <HomeContainer data={data} planetas={planetas}/>
     </Layout>
   );
 }
 
 export async function getServerSideProps({ res }) {
   const data = await getDataCollection('ultimas_entradas?limit=8', res);
-  return { props: { data } };
+  const planetas = await getDataCollection('planetas', res);
+  return { props: { data, planetas } };
 }
-
 
 export default Home;
