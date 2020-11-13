0494a49dc306e1c1663000d1847844b7d60edfd6
Nayla <nayla.mar2@gmail.com>
2020-11-13 09:19:56 -0300

    Update styles.js

--- containers/PersonajesUniverso/styles.js
+++ containers/PersonajesUniverso/styles.js
@@ -74,8 +74,8 @@ export const ContImg = styled.div`
 `;
 
 export const ImagenPersonaje = styled.img`
-    min-width: 100%;
-    min-height: 100%;
+  min-width: 100%;
+  min-height: 100%;
 `;
 
 export const PersonajeInfo = styled.div`
